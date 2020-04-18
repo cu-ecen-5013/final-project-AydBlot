f2a6fbdacb28c16051400cad9d26907eab1cf3cf
Martin Lennartz <male2376@colorado.edu>
2020-04-18 13:10:23 -0600

    Updated LCD Driver Version hash and modified the BUILD_CMD from calling make `all` to `default` this will preserve the current functionality. The make call `all` now will build the utility in three different places (root dir, lcd-util and prJ_base sub-dirs).  The point to is to not break the buildroot directory and have Nick or Ayden test.  This can be tested by change the 'default' to 'all' and attempting to build buildroot.

--- base_external/package/lcd_driver/lcd-driver.mk
+++ base_external/package/lcd_driver/lcd-driver.mk
@@ -1,15 +1,15 @@
-LCD_DRIVER_VERSION = d1f980c84412b1524cc4704e5bbaecda88c861aa
+LCD_DRIVER_VERSION = c0ea34e8f4bc54e53dd639373ebf61876b96de66
 LCD_DRIVER_SITE = git@github.com:cu-ecen-5013/final-project-ssssNick.git
 LCD_DRIVER_SITE_METHOD = git
 
 $(eval $(kernel-module))
 
 define LCD_DRIVER_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) default
 endef
 
 define LCD_DRIVER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/aesd_lcd_util $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd_lcd_util_sp1 $(TARGET_DIR)/usr/bin
 endef
 
 LCD_DRIVER_MODULE_SUBDIRS = /lcd-driver
