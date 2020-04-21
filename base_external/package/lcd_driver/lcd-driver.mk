8b93e7e2733c63ec50777d48124ce2c86ee2f615
Martin Lennartz <male2376@colorado.edu>
2020-04-21 15:48:17 -0600

    Updated with latest hash and changed make call to 'all' and installing correct lcd utility.

--- base_external/package/lcd_driver/lcd-driver.mk
+++ base_external/package/lcd_driver/lcd-driver.mk
@@ -1,15 +1,15 @@
-LCD_DRIVER_VERSION = ed23b55150cfc10754217ca4da3444978a94b26c
+LCD_DRIVER_VERSION = ead1311831418d9ca1d754009167c39d83c5375b
 LCD_DRIVER_SITE = git@github.com:cu-ecen-5013/final-project-ssssNick.git
 LCD_DRIVER_SITE_METHOD = git
 
 $(eval $(kernel-module))
 
 define LCD_DRIVER_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) default
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 define LCD_DRIVER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/aesd_lcd_util_sp1 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/lcd-util/aesd_lcd_util $(TARGET_DIR)/usr/bin
 endef
 
 LCD_DRIVER_MODULE_SUBDIRS = /lcd-driver
