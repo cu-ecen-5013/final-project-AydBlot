0ce999d2470b541d6408defeb037b8818789fff8
Ayden <jobl9833@colorado.edu>
2020-04-23 14:11:59 -0600

    Added automated key generation script, firewall rules including NAT forwarding, and updated easyrsa configuration. openVPN now allows client to connect to internet.

--- base_external/rootfs/usr/startupscripts/firewall.sh
+++ base_external/rootfs/usr/startupscripts/firewall.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 1194 -j ACCEPT
+iptables -A INPUT -i wlan0 -m state --state NEW -p udp --dport 1194 -j ACCEPT
 
 #Allow TUN interface connections to OpenVPN server
 iptables -A INPUT -i tun+ -j ACCEPT
 
 #Allow TUN interface connections to be forwarded through other interfaces
 iptables -A FORWARD -i tun+ -j ACCEPT
-iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -m conntrack --cstate NEW -i tun+ -s 10.5.0.0/24 -d 192.168.1.0/24 -o wlan0 -j ACCEPT
+iptables -A FORWARD -m conntrack --cstate NEW -i wlan0 -s 192.168.1.0/24 -d 10.5.0.0/24 -o tun+ -j ACCEPT
 
 #NAT the VPN client traffic to the Internet. change the ip address mask according to your info of tun0 result while running "ifconfig" command.
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.5.0.0/24 -o wlan0 -j MASQUERADE
 
 iptables -A OUTPUT -o tun+ -j ACCEPT
 
+iptables-save
--- base_external/rootfs/usr/startupscripts/vpn_setup.sh
+++ base_external/rootfs/usr/startupscripts/vpn_setup.sh
@@ -1,31 +1,38 @@
-#!/bin/bash
+#!/bin/sh
+LOGFILE=/var/log/startup-openvpn-key-gen.log
+if grep -Fxq FINISHED $LOGFILE
+then
+	echo "openVPN keys already established"
+	exit 0
+fi
 cd /etc/openvpn/easy-rsa/
 echo "Initializing Public Key Infrastructure"
-./easyrsa init-pki
+./easyrsa init-pki >> $LOGFILE
 echo "Building CA key"
-echo "Type a password for your CA key and a common name" 
-./easyrsa build-ca
+./easyrsa build-ca nopass >> $LOGFILE 
 echo "Building the server key"
-./easyrsa gen-req vpn-server nopass
+./easyrsa gen-req vpn-server nopass >> $LOGFILE 
 echo "Signing the server key with the CA, please enter the CA password entered above"
-./easyrsa sign-req server vpn-server
+./easyrsa sign-req server vpn-server >> $LOGFILE 
 echo "Building the client key"
-./easyrsa gen-req client01 nopass
+./easyrsa gen-req client01 nopass >> $LOGFILE 
 echo "Signing the client key with the CA, please enter the CA pasword entered above"
-./easyrsa sign-req client client01
+./easyrsa sign-req client client01 >> $LOGFILE 
 
-echo "Generate the Diffie-Hellman key"
-./easyrsa gen-dh
+echo "Generate the Diffie-Hellman key, this may take a while"
+./easyrsa gen-dh >> $LOGFILE 
 echo "Generate Certificate Revoking List"
-./easyrsa gen-crl
+./easyrsa gen-crl nopass >> $LOGFILE 
 
-cp pki/ca.crt /etc/openvpn/server/
-cp pki/issued/vpn-server.crt /etc/openvpn/server/
-cp pki/private/vpn-server.key /etc/openvpn/server/
-cp pki/dh.pem /etc/openvpn/server/
-cp pki/crl.pem /etc/openvpn/server/
+cp pki/ca.crt /etc/openvpn/server/ >> $LOGFILE 
+cp pki/issued/vpn-server.crt /etc/openvpn/server/ >> $LOGFILE 
+cp pki/private/vpn-server.key /etc/openvpn/server/ >> $LOGFILE 
+cp pki/dh.pem /etc/openvpn/server/ >> $LOGFILE 
+cp pki/crl.pem /etc/openvpn/server/ >> $LOGFILE 
 
-cp pki/ca.crt /etc/openvpn/client/
-cp pki/issued/client01.crt /etc/openvpn/client/
-cp pki/private/client01.key /etc/openvpn/client/
+cp pki/ca.crt /etc/openvpn/client/ >> $LOGFILE 
+cp pki/issued/client01.crt /etc/openvpn/client/ >> $LOGFILE 
+cp pki/private/client01.key /etc/openvpn/client/ >> $LOGFILE 
 
+openvpn --config /etc/openvpn/server/server.conf
+echo "FINISHED" >> $LOGFILE 
