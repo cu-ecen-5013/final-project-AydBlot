5d5552201e384bbf02268a62a00cb8afb4af7621
Nick Brubaker <brubakernick@gmail.com>
2020-04-25 12:49:10 -0600

    Adding install of test program

--- base_external/package/lcd_driver/lcd-driver.mk
+++ base_external/package/lcd_driver/lcd-driver.mk
@@ -1,4 +1,4 @@
-LCD_DRIVER_VERSION = 101b955f7181543f22983ba2ffe6362e8ab86bdf
+LCD_DRIVER_VERSION = 3e70f751216213f4aa8f99b245ab99ae921ce007
 LCD_DRIVER_SITE = git@github.com:cu-ecen-5013/final-project-ssssNick.git
 LCD_DRIVER_SITE_METHOD = git
 
@@ -10,6 +10,7 @@ endef
 
 define LCD_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/lcd-util/aesd_lcd_util $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/lcd-test/lcd_test $(TARGET_DIR)/usr/bin
 endef
 
 LCD_DRIVER_MODULE_SUBDIRS = /lcd-driver
